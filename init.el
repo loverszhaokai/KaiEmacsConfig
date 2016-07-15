ef4122b01921b367a0bf5f1beb156bbf2c6c7b66
zhaokai <zhaokai@yidian-inc.com>
2016-07-15 20:18:31 +0800

    Set column 80

--- init.el
+++ init.el
@@ -21,6 +21,7 @@
 (require 'fill-column-indicator)
 (add-hook 'c-mode-hook 'fci-mode)
 (add-hook 'c++-mode-hook 'fci-mode)
+(setq fci-rule-column 80)
 (setq fci-rule-color "red")
 (setq fci-rule-width 2)
 
